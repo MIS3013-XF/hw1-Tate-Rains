6aa539c84ab72f2fe06d004ad1389df611304685
Tate Rains <Tatum.B.Rains-1@ou.edu>
2025-09-10 10:53:07 -0500

    Homework 1 Finish

--- HW1a_Sales/Program.cs
+++ HW1a_Sales/Program.cs
@@ -1,6 +1,6 @@
 ﻿// HW1a Sales Total
 
-// Your Name:
+// Tate Rains:
 // Did you seek help ? If yes, specify the helper or web link here: 
 
 using System;
@@ -15,6 +15,41 @@ namespace HW1a_Sales
     {
         static void Main(string[] args)
         {
+            double num1;
+            double num2;
+
+            double sum;
+            double sum2;
+            double sum3;
+            const double multiplier = 0.085;
+
+            string productAsString;
+            string num1AsString;
+            string num2AsString;
+
+            Console.WriteLine("What is the product name of the item you are purchasing?");
+            productAsString = Console.ReadLine();
+            Console.WriteLine($"How many {productAsString} do you want to buy?");
+            num1AsString = Console.ReadLine();
+            Console.WriteLine($"What is the price for each {productAsString}?");
+            num2AsString = Console.ReadLine();
+
+            num1 = Convert.ToDouble(num1AsString);
+            num2 = Convert.ToDouble(num2AsString);
+
+            sum = num1 * num2;
+
+            Console.WriteLine($"Your subtotal for your bill is ${sum}");
+
+            sum2 = sum * multiplier;
+
+            Console.WriteLine($"Your sales tax for your bill is ${sum2}");
+
+            sum3 = sum + sum2;
+
+            Console.WriteLine($"Your total for your bill is ${sum3}");
+
+            Console.ReadKey();
         }
     }
 }
